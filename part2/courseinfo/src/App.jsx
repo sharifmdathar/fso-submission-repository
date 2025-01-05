763781443a307a0cb8be02e274137ce4ac8d9251
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-01-05 16:09:44 +0530

    2.1: Course information step 6
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,48 @@
+const Header = ({ courseName }) => <h1>{courseName}</h1>;
+
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+);
+const Content = ({ courseParts }) => (
+  <>
+    {courseParts.map((part) => (
+      <Part key={part.id} name={part.name} exercises={part.exercises} />
+    ))}
+  </>
+);
+
+const Course = ({ course }) => (
+  <>
+    <Header courseName={course.name} />
+    <Content courseParts={course.parts} />
+  </>
+);
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
