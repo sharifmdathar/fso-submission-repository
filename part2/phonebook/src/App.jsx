b0f027d637f36da560c571a4f77768ea25183bba
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-01-05 18:10:51 +0530

    2.10: The Phonebook Step 5
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,51 @@
 import { useState } from "react";
 
+const Filter = ({ setFilter }) => (
+  <p>
+    filter shown with <input onChange={(e) => setFilter(e.target.value)} />
+  </p>
+);
+
+const AddNewNumberForm = ({
+  handleSubmit,
+  newName,
+  handleNameChange,
+  newNumber,
+  handleNumberChange,
+}) => (
+  <>
+    <h2>add a new</h2>
+    <form onSubmit={handleSubmit}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  </>
+);
+
+const Details = ({ person }) => (
+  <p>
+    {person.name} {person.number}
+  </p>
+);
+
+const ShowNumbers = ({ persons, filter }) => (
+  <>
+    <h2>Numbers</h2>
+    {persons.map((person) => {
+      if (person.name.toLowerCase().search(filter.toLowerCase()) !== -1) {
+        return <Details key={person.id} person={person} />;
+      }
+    })}
+  </>
+);
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456", id: 1 },
@@ -20,53 +66,30 @@ const App = () => {
     ) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }));
+      setPersons(
+        persons.concat({
+          name: newName,
+          number: newNumber,
+          id: persons.length + 1,
+        })
+      );
       setNewName("");
       setNewNumber("");
     }
   };
 
-  const handleNameChange = (e) => {
-    setNewName(e.target.value);
-  };
-
-  const handleNumberChange = (e) => {
-    setNewNumber(e.target.value);
-  };
-
-  const handleFilterChange = (e) => {
-    setFilter(e.target.value);
-  };
-
   return (
     <div>
       <h2>Phonebook</h2>
-      <p>
-        filter shown with <input onChange={handleFilterChange} />
-      </p>
-      <h2>add a new</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map((person) => {
-        if (person.name.toLowerCase().search(filter.toLowerCase()) !== -1) {
-          return (
-            <p key={person.name}>
-              {person.name} {person.number}
-            </p>
-          );
-        }
-      })}
+      <Filter setFilter={setFilter} />
+      <AddNewNumberForm
+        handleSubmit={handleSubmit}
+        newName={newName}
+        handleNameChange={(e) => setNewName(e.target.value)}
+        newNumber={newNumber}
+        handleNumberChange={(e) => setNewNumber(e.target.value)}
+      />
+      <ShowNumbers persons={persons} filter={filter} />
     </div>
   );
 };
