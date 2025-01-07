245d0e7ed982107ce27b85902154423a39e5fde0
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-01-07 19:53:38 +0530

    2.11: The Phonebook Step 6
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Filter = ({ setFilter }) => (
   <p>
@@ -47,15 +48,18 @@ const ShowNumbers = ({ persons, filter }) => (
 );
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      const data = response.data;
+      setPersons(data);
+    });
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
