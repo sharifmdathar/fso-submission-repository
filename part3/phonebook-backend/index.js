ec8153f16b517c905457ad1af80f083256be1be6
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-01-09 19:38:50 +0530

    3.4: Phonebook backend step 4
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -37,10 +37,16 @@ app.get("/info", (req, res) => {
 
 app.get("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  const person = persons.find(p => p.id == id)
-  if (person) res.json(person)
-  res.status(404).end()
-})
+  const person = persons.find((p) => p.id === id);
+  if (person) res.json(person);
+  res.status(404).end();
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((p) => p.id !== id);
+  res.status(204).end();
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
