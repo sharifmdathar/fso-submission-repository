67c6a52479987f97fa0d13f7f4235631e3756693
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-01-09 21:50:25 +0530

    3.8*: Phonebook backend step 8
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const morgan = require('morgan')
+const morgan = require("morgan");
 const app = express();
 app.use(express.json());
-app.use(morgan("tiny"))
+
+morgan.token("body", (req, res) => {
+  if (req.method === "POST") return JSON.stringify(req.body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
