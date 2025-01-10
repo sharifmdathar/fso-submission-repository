073564d926c7e3cb7c6b0df95bf63d6a66208fc7
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-01-10 20:07:59 +0530

    3.9 Phonebook backend step 9
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 morgan.token("body", (req, res) => {
   if (req.method === "POST") return JSON.stringify(req.body);
 });
@@ -71,7 +72,7 @@ app.post("/api/persons", (req, res) => {
   res.json(newPerson);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("App is listening on PORT", PORT);
 });
