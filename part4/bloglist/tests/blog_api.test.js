0a67f12a9f9332b4d081ce8698f1ce39991004c8
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-01-12 20:56:30 +0530

    feat: set up beforeEach in tests so that db is resetted everytime tests are run
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,10 +1,24 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach } = require("node:test");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 
+const Blog = require("../models/blog")
 const app = require("../app");
 const api = supertest(app);
+
+const initialBlogs = [
+  { title: "Example1", author: "Mr. Example1", url: "example1.com", likes: 1 },
+  { title: "Example2", author: "Mr. Example2", url: "example2.com", likes: 2 },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  const blogObjects = initialBlogs.map((blog) => new Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
+  await Promise.all(promiseArray);
+});
 
 test("all blogs are returned and are in JSON format", async () => {
   const response = await api
