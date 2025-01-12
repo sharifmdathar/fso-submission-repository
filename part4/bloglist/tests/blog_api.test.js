e6ab1868b0f8f7f537278f609c63ea8b77d61705
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-01-12 20:09:54 +0530

    4.9: Blog List Tests, step 2
    Write a test that verifies that the unique identifier property of the blog posts is named id, by default the database names the property _id.
    
    Make the required changes to the code so that it passes the test. The toJSON method discussed in part 3 is an appropriate place for defining the id parameter.
    
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -6,13 +6,19 @@ const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
 
-test("all blogs are returned", async () => {
+test("all blogs are returned and are in JSON format", async () => {
   const response = await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
 
   assert.strictEqual(response.body.length, 2);
+});
+
+test("unique identifier is named id instead of _id", async () => {
+  const response = await api.get("/api/blogs");
+  const blogs = response.body
+  assert(blogs.reduce((_, blog) => blog.id !== undefined, false))
 });
 
 after(async () => {
