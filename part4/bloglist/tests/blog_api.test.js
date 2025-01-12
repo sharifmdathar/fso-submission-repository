bc7f6fe3c4ec1d975b065a084f02ed8b4998efd0
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-01-12 19:49:06 +0530

    4.8: Blog List Tests, step 1
    Use the SuperTest library for writing a test that makes an HTTP GET request to the /api/blogs URL. Verify that the blog list application returns the correct amount of blog posts in the JSON format.
    
    Once the test is finished, refactor the route handler to use the async/await syntax instead of promises.
    
    Notice that you will have to make similar changes to the code that were made in the material, like defining the test environment so that you can write tests that use separate databases.
    
    NB: when you are writing your tests it is better to not execute them all, only execute the ones you are working on.
    
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,20 @@
+const { test, after } = require("node:test");
+const assert = require("node:assert");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+
+const app = require("../app");
+const api = supertest(app);
+
+test("all blogs are returned", async () => {
+  const response = await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  assert.strictEqual(response.body.length, 2);
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
