89e2c4e0a37b9698adff85053af4fa1c191673c8
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-01-12 20:20:40 +0530

    4.10: Blog List Tests, step 3
    Write a test that verifies that making an HTTP POST request to the /api/blogs URL successfully creates a new blog post. At the very least, verify that the total number of blogs in the system is increased by one. You can also verify that the content of the blog post is saved correctly to the database.
    
    Once the test is finished, refactor the operation to use async/await instead of promises.
    
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -17,8 +17,31 @@ test("all blogs are returned and are in JSON format", async () => {
 
 test("unique identifier is named id instead of _id", async () => {
   const response = await api.get("/api/blogs");
-  const blogs = response.body
-  assert(blogs.reduce((_, blog) => blog.id !== undefined, false))
+  const blogs = response.body;
+  assert(blogs.reduce((_, blog) => blog.id !== undefined, false));
+});
+
+test("a new blog post can be created", async () => {
+  let response = await api.get("/api/blogs");
+  const prevBlogsAmount = response.body.length;
+
+  const newBlog = {
+    title: "async/await simplifies making async calls",
+    author: "ES7",
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  response = await api.get("/api/blogs");
+  const newBlogsAmount = response.body.length;
+  assert.strictEqual(newBlogsAmount, prevBlogsAmount + 1);
+
+  const titles = response.body.map((blog) => blog.title);
+  assert(titles.includes("async/await simplifies making async calls"));
 });
 
 after(async () => {
