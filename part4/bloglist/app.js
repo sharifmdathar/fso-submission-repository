ba91757dce86eb4000b9bda0399f20344dbb8040
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-01-13 12:55:00 +0530

    4.15: Blog List Expansion, step 3 Implement a way to create new users by doing an HTTP POST request to address api/users. Users have a username, password and name.
    Do not save passwords to the database as clear text, but use the bcrypt library like we did in part 4 chapter Creating users.
    
    Implement a way to see the details of all users by doing a suitable HTTP request.
    
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const blogRouter = require("./controller/blogRouter");
+const usersRouter = require("./controller/userRoutes");
 
 const app = express();
 mongoose.connect(config.MONGODB_URI);
@@ -10,5 +11,6 @@ mongoose.connect(config.MONGODB_URI);
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
