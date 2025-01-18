63c63aede489577a084ec3ac30b11683fd3ecc77
sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>
2025-01-18 14:52:33 +0530

    5.7 Blog List Frontend, step 7
    Let's add a button to each blog, which controls whether all of the details about the blog are shown or not.
    
    Full details of the blog open when the button is clicked.
    
    And the details are hidden when the button is clicked again.
    
    At this point, the like button does not need to do anything.
    
    The application shown in the picture has a bit of additional CSS to improve its appearance.
    
    NB: Even though the functionality implemented in this part is almost identical to the functionality provided by the Togglable component, it can't be used directly to achieve the desired behavior. The easiest solution would be to add a state to the blog component that controls if the details are being displayed or not.
    
    Signed-off-by: sharifmdathar <116189751+sharifmdathar@users.noreply.github.com>

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{" "}
+      <button onClick={() => setShowDetails(!showDetails)}>view</button>
+      {showDetails && (
+        <>
+          <p>{blog.url}</p>
+          <p>
+            likes {blog.likes} <button>like</button>
+          </p>
+          <p>{blog.user.name}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
